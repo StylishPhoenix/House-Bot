287a3bc89d41d96d5040564fea087b6754b91e6f
StylishPhoenix <an007567@gmail.com>
2023-04-10 18:05:16 -0500

    Update HouseBot.js

--- HouseBot.js
+++ HouseBot.js
@@ -146,13 +146,14 @@ function addPointsForUser(house, points) {
 async function updateVoiceChannelPoints(guild) {
   setInterval(async () => {
     const voiceChannels = guild.channels.cache.filter((channel) => channel.type === 2 && channel.id !== guild.afkChannelId);
-    console.log('Voice channels found:');
     for (const voiceChannel of voiceChannels.values()) {
-      console.log(voiceChannel.name);
       // Check if there are more than 1 human members in the voice channel
       const humanMembers = voiceChannel.members.filter(member => !member.user.bot && !member.voice.mute && !member.voice.deaf);
       if (humanMembers.size >= minimumVoice) {
         for (const member of humanMembers.values()) {
+	  console.log(`User ${member.id} found in voice channel ${voiceChannel.name}`);
+	  const roles = member.roles.cache.map(role => role.name);
+          console.log(`User ${member.id} has roles: ${roles.join(', ')}`);
           const house = await getUserHouse(guild, member.id);
           if (house) {
             addPointsForUser(house, pointsPerInterval);
