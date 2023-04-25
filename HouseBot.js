e892049e5c4af3c7d62887b0683076ca79bcefee
StylishPhoenix <an007567@gmail.com>
2023-04-24 19:45:53 -0500

    Update HouseBot.js

--- HouseBot.js
+++ HouseBot.js
@@ -254,7 +254,7 @@ async function logPoints(userId, house, points, reason) {
 }
 
 async function displayLeaderboard(interaction, house, client) {
-  // try{
+ try{
     // Retrieve the leaderboard data from the database
   const leaderboardData = await getLeaderboardData(house);
 
@@ -278,9 +278,9 @@ async function displayLeaderboard(interaction, house, client) {
 
   // Send the embed as a reply
   await interaction.reply({ embeds: [embed] });
- // } catch {
-   // await interaction.reply("It doesn't appear that this house has history yet.");
-//  }
+ } catch {
+    await interaction.reply("It doesn't appear that this house has history yet.");
+ }
 }
 
 function createTableIfNotExists(db) {
